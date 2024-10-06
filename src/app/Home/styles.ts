833fe6d4ba366fbca8d9470322e339058504c939
gustavogss <gustavogss.dev@gmail.com>
2024-10-06 01:05:32 -0300

    Header Create

--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -1,11 +1,9 @@
 import {StyleSheet} from "react-native"
-import { colors } from "../../styles/colors";
+import { colors } from "@/styles/colors";
 
 export const styles = StyleSheet.create({
  container:{
   flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center'
  },
  title:{
   fontSize: 20,
